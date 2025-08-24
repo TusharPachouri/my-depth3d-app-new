7b4188833243a756878b583913e341281e483e50
Tushar Pachouri <tusharpachouri001@gmail.com>
2025-08-24 10:05:56 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Depth3D from '$lib/Depth3D';
+</script>
+
+<main class="w-full h-screen flex items-center justify-center bg-gray-100">
+  <div class="w-96 h-96">
+    <!-- The component will adjust to parent size -->
+    <Depth3D
+      image={{
+        image: '/images/your-image.jpg',
+        depth: '/images/your-depth-map.png'
+      }}
+    />
+  </div>
+</main>
